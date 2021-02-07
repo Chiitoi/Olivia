eddbfe890826e94cceb51243dab4036922beb4c1
Chiitoi <vrajakul@gmail.com>
2021-02-06 19:27:51 -0500

    Fixed "ready" event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,13 +2,15 @@ import { ApplyOptions } from '@sapphire/decorators'
 import { Event, EventOptions, Events } from '@sapphire/framework'
 
 @ApplyOptions<EventOptions>({
-    event: Events.GuildDelete,
+    event: Events.Ready,
     once: true
 })
 export default class extends Event<Events.Ready> {
     public async run() {
         const { client } = this.context!
-        const emojiCount = client.emojis.cache.size
-        await client.user.setActivity(`over ${ emojiCount } emoji(s)!`, { type: 'WATCHING' })
+                
+        await client.user.setActivity(`a ton of emojis!`, { type: 'WATCHING' })
+
+        console.log(`${ client.user.tag } is online!`)
     }
 }
